fc5a11b9b1df2ff5d5d2878789349451491f9c0e
Renat Buryachenko <renat.buryachenko@gmail.com>
2019-10-02 22:38:19 +0400

    подточил цвет у BottomNavigation

--- app/src/main/java/ru/buryachenko/hw_look4films/activities/MainActivity.java
+++ app/src/main/java/ru/buryachenko/hw_look4films/activities/MainActivity.java
@@ -18,7 +18,6 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
